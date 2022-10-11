2a3e138438f4727d957596b384c168c62548820e
Mohammadibrahim2 <mohammadibrahim6454@gmail.com>
2022-10-12 02:49:06 +0600

    finishing the quize topis section by grid layout

--- src/component/Quizes/TopicBasedQuizes/TopicBasedQuizes.js
+++ src/component/Quizes/TopicBasedQuizes/TopicBasedQuizes.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import Options from "../Options/Options";
 import { EyeIcon,ArrowRightIcon} from '@heroicons/react/24/solid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const TopicBasedQuizes=({quiz})=>{
 
     const {name,question,correctAnswer,options}=quiz
 
-   const checkCorrect=()=>{
-    alert(correctAnswer)
-   }
+    const notify = () => toast("the correct answer is :" correctAnswer);
+  
     const testResult=(ans)=>{
         const rightAwnser=ans.option
 
@@ -29,9 +30,13 @@ return(
 <div className="bg-fuchsia-100 my-2 py-3 px-9">
 <div className="flex justify-between">
 <h4 className="text-2xl my-3 flex align-baseline "><ArrowRightIcon  className="w-6 h-6"></ArrowRightIcon> {question}</h4>
-<button onClick={checkCorrect}>
+
+<button onClick={notify}>
+        
 <EyeIcon className="w-5 h-5"></EyeIcon>
- </button>
+</button>
+<ToastContainer />
+
 </div>
 
 
